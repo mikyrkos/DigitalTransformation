c2546755ff80a3924aae44590af458fffcbb6b46
mike <mi.kyrkos@gmail.com>
2026-04-30 17:00:01 +0300

    Add Cat class and ISpeakable interface for animal speaking functionality

---
+++ src/gr/aueb/dt/ch5/Cat.java
@@ -0,0 +1,25 @@
+package gr.aueb.dt.ch5;
+
+public class Cat implements ISpeakable {
+
+    private String name;
+
+    @Override
+    public void speak() {
+        System.out.println("Meow");
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/gr/aueb/dt/ch5/ISpeakable.java
@@ -0,0 +1,7 @@
+package gr.aueb.dt.ch5;
+
+public interface ISpeakable {
+
+    void speak();
+
+}
